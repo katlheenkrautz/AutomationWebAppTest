c8cdc6f914c4daa5dda48b40d9baf375229f4692
Katlheen Krautz <98427130+katlheenkrautz@users.noreply.github.com>
2025-07-13 18:16:39 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,18 @@
+# BASE_URL is the base URL of the server. 
+# To get it, you need to start the server, copy the URL it provides, and paste it here. (This is just an example.)
 urban_routes_url = ' https://cnt-db4e6c56-7e38-41d5-b496-f40dae988370.containerhub.tripleten-services.com?lng=es'
+
+# Starting address for the route
 address_from = 'East 2nd Street, 601'
+
+# Destination address for the route
 address_to = '1300 1st St'
+
+# User's phone number (used for booking or contact purposes)
 phone_number = '+1 123 123 12 12'
+
+# Payment card information (example data: card number and security code)
 card_number, card_code = '1234 5678 9100', '111'
+
+# Optional message for the driver (in Spanish)
 message_for_driver = 'Muéstrame el camino al museo'
